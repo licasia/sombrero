1031171feff4a959131e12d8f38067af81efbdb3
sfo <sfo@ca>
2026-02-05 17:58:23 +0800

    defining structure

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,47 @@
 import { OpenRouter } from '@openrouter/sdk';
 import 'dotenv/config';
 
-const client = new OpenRouter({
-  apiKey: process.env.OPENROUTER_API_KEY
-});
-const response = await client.chat.send({
-  model: "minimax/minimax-m2",
-  messages: [
-    { role: "user", content: "Explain quantum computing" }
-  ]
-});
+const API_KEY = process.env.OPENROUTER_API_KEY;
+const MODEL = 'tngtech/deepseek-r1t2-chimera:free';
+const SCHEMA = {
+  "type": "array",
+  "items": {
+    "type": "object",
+    "properties": {
+      "id": { "type": "number" },
+      "extractedName": { "type": "string" }
+    },
+    "required": ["id", "extractedName"],
+    "additionalProperties": false
+  }
+};
+
+class Sombrero {
+  getOpenRouter(apiKey) {
+    return openRouter = new OpenRouter({
+      apiKey: apiKey
+    });
+  }
+
+  async fetchResult() {
+    return Sombrero.fetchResult(this.prompt, this.model);
+  }
+
+  static async fetchResult(prompt, model) {
+    const completion = await openRouter.chat.send({
+      model: model,
+      messages: [
+        {
+          role: 'user',
+          content: prompt,
+        },
+      ],
+      response_format: {
+        type: "json_schema",
+        json_schema: jsonSchema
+      },
+      stream: false,
+    });
+    return completion?.choices?.[0]?.message?.content
+  }
+}
